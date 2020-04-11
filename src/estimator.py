705fcbdc18364b0add065f936352c6fa67efa207
arsenho <anvadele@yahoo.com>
2020-04-11 16:05:25 +0100

    some changes for challenge 2

--- src/estimator.py
+++ src/estimator.py
@@ -19,29 +19,36 @@ def estimator(data):
     """
     data: {},  the input data you got
     impact: {},  your best case estimation
-     severeImpact: {} your severe case estimation
+    severeImpact: {} your severe case estimation
     """
     impact = {}
     severeImpact = {}
     if isinstance(data, dict):
-
+        # Challenge 1
         impact["currentlyInfected"] = int(data["reportedCases"]) * 10
         impact["infectionsByRequestedTime"] = (int(impact["currentlyInfected"]) * pow(2,
                                                                                       (converter(data) / 3) // 1)) // 1
+        severeImpact["currentlyInfected"] = int(data["reportedCases"]) * 50
+        severeImpact["infectionsByRequestedTime"] = (int(severeImpact["currentlyInfected"]) * pow(2, (
+                converter(data) / 3) // 1)) // 1
+
+        # Start of Challenge 2
         impact["severeCasesByRequestedTime"] = (0.15 * impact["infectionsByRequestedTime"]) // 1
-        impact["hospitalBedsByRequestedTime"] = ((((int(data.get("totalHospitalBeds"))) * 0.925) * 0.35) - impact[
+        impact["hospitalBedsByRequestedTime"] = (((int(data.get("totalHospitalBeds"))) * 0.35) - impact[
             "severeCasesByRequestedTime"]) // 1
+
+        severeImpact["severeCasesByRequestedTime"] = (0.15 * severeImpact["infectionsByRequestedTime"]) // 1
+        severeImpact["hospitalBedsByRequestedTime"] = ((((int(data.get("totalHospitalBeds"))) * 0.925) * 0.35) -
+                                                       severeImpact["severeCasesByRequestedTime"]) // 1
+
+        # End of Challenge 2
+
+        # Challenge 3
         impact["casesForICUByRequestedTime"] = (0.05 * impact["infectionsByRequestedTime"]) // 1
         impact["casesForVentilatorsByRequestedTime"] = (0.02 * impact["infectionsByRequestedTime"]) // 1
         impact["dollarsInflight"] = (impact["infectionsByRequestedTime"] * data["region"]["avgDailyIncomeInUSD"] * \
                                      data["region"]["avgDailyIncomePopulation"] * converter(data)) // 1
 
-        severeImpact["currentlyInfected"] = int(data["reportedCases"]) * 50
-        severeImpact["infectionsByRequestedTime"] = (int(severeImpact["currentlyInfected"]) * pow(2, (
-                converter(data) / 3) // 1)) // 1
-        severeImpact["severeCasesByRequestedTime"] = (0.15 * severeImpact["infectionsByRequestedTime"]) // 1
-        severeImpact["hospitalBedsByRequestedTime"] = ((((int(data.get("totalHospitalBeds"))) * 0.925) * 0.35) -
-                                                       severeImpact["severeCasesByRequestedTime"]) // 1
         severeImpact["casesForICUByRequestedTime"] = (0.05 * severeImpact["infectionsByRequestedTime"]) // 1
         severeImpact["casesForVentilatorsByRequestedTime"] = (0.02 * severeImpact["infectionsByRequestedTime"]) // 1
         severeImpact["dollarsInflight"] = (severeImpact["infectionsByRequestedTime"] * data["region"][
