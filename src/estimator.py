dd52a9177834dcea297b1102ec31d1044379ad6c
arsenho <anvadele@yahoo.com>
2020-04-11 14:55:56 +0100

    my tests

--- src/estimator.py
+++ src/estimator.py
@@ -16,6 +16,11 @@ def converter(data):
 
 
 def covid19ImpactEstimator(data):
+    """
+    data: {},  the input data you got
+    impact: {},  your best case estimation
+     severeImpact: {} your severe case estimation
+    """
     impact = {}
     severeImpact = {}
     if isinstance(data, dict):
@@ -51,19 +56,19 @@ def covid19ImpactEstimator(data):
         "severeImpact": severeImpact
     }
 
-# if __name__ == "__main__":
-#   data = {
-#        "region": {
-#            "name": "Africa",
-#            "avgAge": 19.7,
-#            "avgDailyIncomeInUSD": 5,
-#            "avgDailyIncomePopulation": 0.71
-#        },
-#        "periodType": "days",
-#        "timeToElapse": 3,
-#        "reportedCases": 674,
-#        "population": 66622705,
-#        "totalHospitalBeds": 1380614
-#    }
-#
-#    print(covid19ImpactEstimator(data))
+if __name__ == "__main__":
+   data = {
+        "region": {
+            "name": "Africa",
+            "avgAge": 19.7,
+            "avgDailyIncomeInUSD": 5,
+            "avgDailyIncomePopulation": 0.71
+        },
+        "periodType": "days",
+        "timeToElapse": 3,
+        "reportedCases": 674,
+        "population": 66622705,
+        "totalHospitalBeds": 1380614
+   }
+   for key, value in covid19ImpactEstimator(data).items():
+       print("{}:{}".format(key, value))
